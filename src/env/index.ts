cced258b1d97ca62cd02f7f2afce3cc980e7216f
geversonfernandes <gever.fernandes@gmail.com>
2023-11-02 03:47:42 -0300

    eslint configuted

--- src/env/index.ts
+++ src/env/index.ts
@@ -8,7 +8,7 @@ const envSchema = z.object({
 
 const _env = envSchema.safeParse(process.env)
 
-if (_env.success === false){
+if (_env.success === false) {
   console.error('❌Invalid enviroment variables!', _env.error.format())
 
   throw new Error('Invalid enviroment variables!')
